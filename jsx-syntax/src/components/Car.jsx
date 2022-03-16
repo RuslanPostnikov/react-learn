6cd992790a3207d4996282b2c75220f97c4cfea6
RuslanPostnikov <rp02081992@gmail.com>
2022-03-16 22:36:50 +0200

    Styles. Radium

--- jsx-syntax/src/components/Car.jsx
+++ jsx-syntax/src/components/Car.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Radium from "radium";
 import './Car.css';
 
 const Car = (props) => {
@@ -8,8 +9,18 @@ const Car = (props) => {
 
     if(props.name.length > 4) inputClasses.push('bold');
 
+    const style = {
+        border: '1px solid #ccc',
+        boxShadow: '0 4px 5px 0 rgba(0, 0, 0, .14)',
+        ':hover': {
+            border: '1px solid #aaa',
+            boxShadow: '0 4px 15px 0 rgba(0, 0, 0, .25)',
+            cursor: 'pointer'
+        }
+    }
+
     return (
-        <div className='Car'>
+        <div className='Car' style={style}>
             <h3>Car name: {props.name}</h3>
             <p>Year <strong>{props.year}</strong></p>
             <input
@@ -23,4 +34,4 @@ const Car = (props) => {
     );
 }
 
-export default Car;
+export default Radium(Car);
