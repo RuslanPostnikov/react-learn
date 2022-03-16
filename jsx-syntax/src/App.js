300b3b443be97d630d3f8991f76a8bd9f955a478
RuslanPostnikov <rp02081992@gmail.com>
2022-03-16 18:02:44 +0200

    jsx-syntax. Props.children

--- jsx-syntax/src/App.js
+++ jsx-syntax/src/App.js
@@ -4,9 +4,12 @@ import Car from "./Car/Car";
 function App() {
   return (
     <div className="App">
-        <Car name={'Ford'} year={2018}/>
-        <Car name={'Audi'} year={2010}/>
-        <Car name={'Mazda'} year={2020}/>
+        <Car name={'Audi'} year={2018}>
+            <p style={{color: 'blue'}}>Test</p>
+        </Car>
+        <Car name={'Mazda'} year={2010}>
+            <p style={{color: 'red'}}>Test</p>
+        </Car>
     </div>
   );
 }
--- jsx-syntax/src/Car/Car.jsx
+++ jsx-syntax/src/Car/Car.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
-const Car = props => (
+const Car = (props) => (
     <div>
         <h3>Car name: {props.name}</h3>
         <p>Year <strong>{props.year}</strong></p>
+        {props.children}
     </div>
 );
 
