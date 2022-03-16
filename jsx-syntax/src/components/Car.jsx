18f212201cf19f00c15f36008951529d37e73d94
RuslanPostnikov <rp02081992@gmail.com>
2022-03-16 18:37:13 +0200

    jsx-syntax. function params

--- jsx-syntax/src/components/Car.jsx
+++ jsx-syntax/src/components/Car.jsx
@@ -4,6 +4,7 @@ const Car = (props) => (
     <div>
         <h3>Car name: {props.name}</h3>
         <p>Year <strong>{props.year}</strong></p>
+        <button onClick={props.onChangeTitle}>Click</button>
     </div>
 );
 
