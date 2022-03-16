ba3137a194a100ef41e6d58df1c9cbf13b05b62c
RuslanPostnikov <rp02081992@gmail.com>
2022-03-16 22:17:52 +0200

    Styles. Dynamic classNames

--- jsx-syntax/src/components/Car.jsx
+++ jsx-syntax/src/components/Car.jsx
@@ -1,13 +1,26 @@
 import React from "react";
 import './Car.css';
 
-const Car = (props) => (
-    <div className='Car'>
-        <h3>Car name: {props.name}</h3>
-        <p>Year <strong>{props.year}</strong></p>
-        <input type="text" onChange={props.onChangeName} value={props.name}/>
-        <button onClick={props.onDelete}>Delete</button>
-    </div>
-);
+const Car = (props) => {
+    const inputClasses = ['input'];
+
+    props.name.length ? inputClasses.push('green') : inputClasses.push('red');
+
+    if(props.name.length > 4) inputClasses.push('bold');
+
+    return (
+        <div className='Car'>
+            <h3>Car name: {props.name}</h3>
+            <p>Year <strong>{props.year}</strong></p>
+            <input
+                type="text"
+                onChange={props.onChangeName}
+                value={props.name}
+                className={inputClasses.join(' ')}
+            />
+            <button onClick={props.onDelete}>Delete</button>
+        </div>
+    );
+}
 
 export default Car;
