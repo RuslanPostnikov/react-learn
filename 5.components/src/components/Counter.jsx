741593ba9aaeafa23672ca9ebf65ca2f4259202c
RuslanPostnikov <rp02081992@gmail.com>
2022-03-17 12:00:27 +0200

    5.Components. Correct state changing

--- 5.components/src/components/Counter.jsx
+++ 5.components/src/components/Counter.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Auxiliary from "../hoc/Auxiliary";
 
 export default class Counter extends React.Component {
     constructor(props) {
@@ -10,18 +9,23 @@ export default class Counter extends React.Component {
     }
 
     addCounter = () => {
-        this.setState({
-            counter: this.state.counter + 1
-        })
+        // this.setState({
+        //     counter: this.state.counter + 1
+        // })
+        this.setState((prevState) => {
+           return {
+               counter: prevState.counter + 1
+           }
+        });
     }
 
     render() {
         return (
-            <Auxiliary>
+            <>
                 <h2>Counter {this.state.counter}</h2>
                 <button onClick={this.addCounter}>+</button>
                 <button onClick={() => {this.setState({counter: this.state.counter - 1})}}>-</button>
-            </Auxiliary>
+            </>
         );
     }
 }
