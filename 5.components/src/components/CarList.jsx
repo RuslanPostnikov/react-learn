d5cafc851fd239420eebaff23220d633f7799855
RuslanPostnikov <rp02081992@gmail.com>
2022-03-17 10:15:45 +0200

    5.Components. Sending params in component

---
+++ 5.components/src/components/CarList.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import './CarList.scss';
+import Car from "./Car";
+
+export default class CarList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            cars: [
+                {name: 'Audi', year: 2018},
+                {name: 'Mazda', year: 2010},
+                {name: 'BMW', year: 2020},
+            ],
+            pageTitle: 'React Components',
+            showCars: false
+        };
+    }
+
+    toggleCarsHandler = () => {
+        this.setState({
+            showCars: !this.state.showCars
+        })
+    }
+
+    onChangeName(name, index) {
+        const car = this.state.cars[index];
+        car.name = name;
+        const cars = [...this.state.cars];
+        cars[index] = car;
+        this.setState({cars})
+    }
+
+    deleteHandler(index) {
+        const cars = this.state.cars.concat();
+        cars.splice(index, 1);
+        this.setState({cars})
+    }
+
+    render() {
+
+        return (
+            <>
+                <h1>{this.props.title}</h1>
+                <button
+                    className={'button'}
+                    onClick={this.toggleCarsHandler}
+                >Toggle cars</button>
+                <div style={{
+                    width: 400,
+                    margin: 'auto',
+                    paddingTop: '20px'
+                }}>
+                    { this.state.showCars ?
+                        this.state.cars.map((car, index) => {
+                            return (
+                                <Car
+                                    key={index}
+                                    name={car.name}
+                                    year={car.year}
+                                    onChangeName={e => this.onChangeName(e.target.value, index)}
+                                    onDelete={this.deleteHandler.bind(this, index)}
+                                />
+                            );
+                        })
+                        : null
+                    }
+                </div>
+            </>
+        );
+    }
+}
