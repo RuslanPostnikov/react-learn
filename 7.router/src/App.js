0f1fe4b1da6ba6f87aba0fb682b48c4c4e681049
RuslanPostnikov <rp02081992@gmail.com>
2022-03-18 12:38:45 +0200

    Router. Navigation between pages

--- 7.router/src/App.js
+++ 7.router/src/App.js
@@ -1,23 +1,28 @@
 import './App.scss';
+import {Route, BrowserRouter, Routes, NavLink} from "react-router-dom";
 import Cars from "./components/Cars";
 import About from "./components/About";
 
 function App() {
   return (
-    <div className="App">
-      <nav className="nav">
-        <ul>
-          <li><a href="/">Home</a></li>
-          <li><a href="/about">About</a></li>
-        </ul>
-      </nav>
+      <BrowserRouter>
+        <div className="App">
+          <nav className="nav">
+            <ul>
+                <li><NavLink to="/">Home</NavLink></li>
+                <li><NavLink to="/about">About</NavLink></li>
+                <li><NavLink to="/cars">Cars</NavLink></li>
+            </ul>
+          </nav>
 
-        <hr/>
-        <About />
-
-        <Cars />
-
-    </div>
+          <hr/>
+            <Routes>
+                <Route path="/" exact element={<h1>Home Page</h1>}/>
+                <Route path="/about" element={<About />} />
+                <Route path="/cars" element={<Cars />}/>
+            </Routes>
+        </div>
+      </BrowserRouter>
   );
 }
 
