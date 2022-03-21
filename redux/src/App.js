2c83d6490683902066acdf5d449b4725141f905c
RuslanPostnikov <rp02081992@gmail.com>
2022-03-21 16:20:32 +0200

    Plug in component

--- redux/src/App.js
+++ redux/src/App.js
@@ -1,24 +1,19 @@
 import React from "react";
+import {connect} from "react-redux";
 import './App.css';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      counter: 0
-    }
-  }
 
   updateCounter(value) {
-    this.setState({
-      counter: this.state.counter + value
-    })
+    // this.setState({
+    //   counter: this.state.counter + value
+    // })
   }
 
   render() {
     return (
         <div className="App">
-          <h1>Counter <strong>{this.state.counter}</strong></h1>
+          <h1>Counter <strong>{this.props.counter}</strong></h1>
           <hr/>
           <div className="Actions">
             <button onClick={() => this.updateCounter(1)}>Add 1</button>
@@ -29,4 +24,10 @@ class App extends React.Component {
   }
 }
 
-export default App;
+function mapStateToProps(state) {
+    return {
+        counter: state.counter
+    }
+}
+
+export default connect(mapStateToProps)(App);
