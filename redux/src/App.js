964879ed64bd0ebd36d80a7cbe26964fcde2f6e1
RuslanPostnikov <rp02081992@gmail.com>
2022-03-21 18:18:26 +0200

    Combining reducers

--- redux/src/App.js
+++ redux/src/App.js
@@ -1,33 +1,39 @@
 import React from "react";
 import {connect} from "react-redux";
 import './App.css';
+import Counter from './Counter'
 
-class App extends React.Component {
-
-    render() {
+const App = props => {
     return (
         <div className="App">
-          <h1>Counter <strong>{this.props.counter}</strong></h1>
-          <hr/>
-          <div className="Actions">
-            <button onClick={() => this.props.onAdd(5)}>Add 1</button>
-            <button onClick={this.props.onSub}>Minus 1</button>
-          </div>
+            <h1>Counter <strong>{props.counter}</strong></h1>
+            <hr/>
+            <div className="Actions">
+                <button onClick={props.onAdd}>Add 1</button>
+                <button onClick={props.onSub}>Minus 1</button>
+            </div>
+
+            <div className="Actions">
+                <button onClick={() => props.onAddNumber(15)}>Add 15</button>
+                <button onClick={() => props.onAddNumber(-17)}>Minus 17</button>
+            </div>
+            <Counter />
         </div>
     )
-    }
+
 }
 
 function mapStateToProps(state) {
     return {
-        counter: state.counter
+        counter: state.counter1.counter
     }
 }
 
 function mapDispatchToProps(dispatch) {
     return {
         onAdd: () => dispatch({type: 'ADD', value: ''}),
-        onSub: () => dispatch({type: 'SUB'})
+        onSub: () => dispatch({type: 'SUB'}),
+        onAddNumber: number => dispatch({type: 'ADD_NUMBER', payload: number})
     }
 }
 
--- redux/src/redux/rootReducer.js
+++ redux/src/redux/rootReducer.js
@@ -1,20 +1,8 @@
+import {combineReducers} from "redux";
 
-const initialState = {
-    counter: 0
-}
+import counter1 from "./reducers/counter1";
+import counter2 from "./reducers/counter2";
 
-export default function rootReducer(state = initialState, action) {
-
-    switch (action.type) {
-        case 'ADD':
-            return {
-                counter: state.counter + 1
-            }
-        case 'SUB':
-            return {
-                counter: state.counter - 1
-            }
-    }
-
-    return state
-}
+export default combineReducers({
+    counter1, counter2
+})
