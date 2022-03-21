4369a25dbf6702318d81bfcc4648a06b909fe3dd
RuslanPostnikov <rp02081992@gmail.com>
2022-03-21 18:31:01 +0200

    Middleware

--- redux/src/index.js
+++ redux/src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {createStore} from "redux";
+import {createStore, applyMiddleware} from "redux";
 import {Provider} from  'react-redux';
 import rootReducer from "./redux/rootReducer";
 
-const store = createStore(rootReducer);
+const loggerMiddleware = store => next => action => {
+    const result = next(action);
+    console.log('Middleware', store.getState());
+    return result
+}
+
+const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
 
 ReactDOM.render(
   <React.StrictMode>
